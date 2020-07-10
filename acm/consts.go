d7f0bb76e76c8206605f409199389681ca9fe677
Jamie Kolles <jamie.kolles@spring-media.com>
2020-07-10 10:14:20 +0200

    added pebble

--- acm/consts.go
+++ acm/consts.go
@@ -2,4 +2,4 @@ package acm
 
 // LetsEncryptStagingURL uri for the LE staging environment with higher rate limits
 // const LetsEncryptStagingURL = "https://acme-staging.api.letsencrypt.org/directory"
-const LetsEncryptStagingURL = "http://localhost:14000"
+const LetsEncryptStagingURL = "https://localhost:14000"
